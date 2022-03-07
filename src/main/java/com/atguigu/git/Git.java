66a615502a567c7484492289d5fd5c686d299f52
zeng <2717379771@qq.com>
2022-03-07 14:35:26 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("Hello Git4");
     }
 }
