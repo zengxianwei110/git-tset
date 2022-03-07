3d35ac0ffa9e445687ad2d5e8ebac6469a7de493
zengxianwei110 <84692559+zengxianwei110@users.noreply.github.com>
2022-03-07 16:43:40 +0800

    pull commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -13,5 +13,6 @@ public class Git {
         System.out.println("master test");
         System.out.println("host-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
