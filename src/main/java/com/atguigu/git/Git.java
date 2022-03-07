f74f040614b26a56f4d703d60531570a3ebab382
zeng <2717379771@qq.com>
2022-03-07 14:27:15 +0800

    second commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
