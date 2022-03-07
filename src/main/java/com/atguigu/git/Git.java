629b630a8d4cbf776c8946c3b18c2ca5a0614b38
zeng <2717379771@qq.com>
2022-03-07 14:25:25 +0800

    fitst commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zxw
+ * @Data 2022/3/7 14:20
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
